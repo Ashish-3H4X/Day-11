9c7e435256e09f14d50539f2f8cb74e62f898019
Mr Rahul <ashish227087@gmail.com>
2025-04-12 10:18:11 +0530

    JavaScript  String Object method

---
+++ project1/script.js
@@ -0,0 +1,19 @@
+
+let r = new String("John");  // x is an object
+let v = new String("John");  // y is an object
+let x = "rahul";
+let y = "rahul"
+ console.log(x==y);
+ console.log(r==v);
+ 
+
+ demo.innerHTML = `let r = new String("John");  // x is an object <br>
+let v = new String("John");  // y is an object <br>
+let x = "rahul"; <br>
+let y = "rahul" <br>
+ console.log(x==y); <br>
+ console.log(r==v);  <br>
+ results`
+
+  demo1.innerHTML = x==y
+  demo2.innerHTML = x==y
