c46efbb97cde63cb1bc0965ba4b71faca8344753
Mr Rahul <ashish227087@gmail.com>
2025-04-12 22:08:03 +0530

    slice() method  string

--- project2/script.js
+++ project2/script.js
@@ -29,9 +29,28 @@ demo4.innerHTML = `var str = "rahul"; <br>
      demo4.innerHTML = str.at(3) <br>
      result : --->>><br>
        ${str.at(3)}`
- /// string text[3] method 
+/// string text[3] method 
 
- demo5.innerHTML = `var str = "rahul"; <br>
+demo5.innerHTML = `var str = "rahul"; <br>
      demo4.innerHTML = str[3] <br>
      result : --->>><br>
        ${str[3]}`
+
+// Slice method 
+let text = "Apple, Banana, Kiwi";
+demo6.innerHTML = ` text = "Apple, Banana, Kiwi"; <br>
+     demo4.innerHTML = text.slice(0,1) <br>
+     result : --->>><br>
+       ${text.slice(0, 1)}`
+demo7.innerHTML = `text = "Apple, Banana, Kiwi"; <br>
+ demo4.innerHTML = text.slice(0,7) <br>
+ result : --->>><br>
+   ${text.slice(0, 7)}`
+demo8.innerHTML = `text = "Apple, Banana, Kiwi"; <br>
+   demo4.innerHTML = text.slice(7,13) <br>
+   result : --->>><br>
+     ${text.slice(7, 13)}`
+demo9.innerHTML = `text = "Apple, Banana, Kiwi"; <br>
+     demo4.innerHTML = text.slice(14, text.length) <br>
+     result : --->>><br>
+       ${text.slice(14, text.length)}`
